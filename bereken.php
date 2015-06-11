1846365a9cdf35e8b1c6e67e1a3694aa795996c0
Arjan de Ruijter <rra@mboutrecht.nl>
2015-06-11 14:57:49 +0200

    JSON object

--- bereken.php
+++ bereken.php
@@ -1,10 +1,43 @@
 <?php
-//Allemaal queries
+// PDO gebruiken voor database contact
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$databaseName = "openhuismboutrecht";
 
-$aantaljongens = 43 + $_POST['id1'];
-$aantalmeisjes = 57 + $_POST['id2'];
-
-$verzendJSONData = '{ "aantaljongens" : "'.$aantaljongens.'", "aantalmeisjes" : "'.$aantalmeisjes.'"}';
+try
+{
+    $connection = new PDO("mysql:host=".$serverName.";dbname=".$databaseName, $userName, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    $queryPrep = $connection->prepare("SELECT `id`, `voornaam`, `tussenvoegsel`,`achternaam` FROM `persoonsgegeven` WHERE `id` < 3");
+    
+    $queryPrep->execute();
+    
+    $queryPrep->setFetchMode(PDO::FETCH_ASSOC);
+    
+    
+    $verzendJSONData = '{ "persoonsgegevens" : [ ';
+    foreach ($queryPrep->fetchAll() as $key => $value)
+    {
+        $verzendJSONData .= '{ "id" : "'.$value["id"].'", "voornaam" : "'.$value["voornaam"].'", "tussenvoegsel" : "'.$value["tussenvoegsel"].'", "achternaam" : "'.$value["achternaam"].'"}, ';
+    }
+    $verzendJSONData .= ' {"id" : "", "voornaam" : "", "tussenvoegsel" : "", "achternaam" : ""}]}';
+    
+    
+    /*
+    $queryPrep->execute();
+    
+    while ($row = $queryPrep->fetch() )
+    {
+        echo $row["id"];
+    }
+    */  
+}
+catch(PDOException $e)
+{
+    echo $e->getMessage();
+}
 
 echo $verzendJSONData;
 
