36e9a85008ca856c4980b92e9d55a3ce61c0dc62
Arjan de Ruijter <rra@mboutrecht.nl>
2015-06-12 11:24:53 +0200

    Verbeterde versie

--- bereken.php
+++ bereken.php
@@ -10,29 +10,37 @@ try
     $connection = new PDO("mysql:host=".$serverName.";dbname=".$databaseName, $userName, $password);
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    $queryPrep = $connection->prepare("SELECT `id`, `voornaam`, `tussenvoegsel`,`achternaam` FROM `persoonsgegeven`");
+    $queryPrep = $connection->prepare("SELECT `id`, `voornaam`, `tussenvoegsel`,`achternaam` FROM `persoonsgegeven` WHERE `id` < 4");
+    
+    $queryPrep1 = $connection->prepare("SELECT COUNT(`geslacht`) AS `aantalvrouwen` FROM `persoonsgegeven` WHERE `geslacht` = 'v'");
     
     $queryPrep->execute();
     
+    $queryPrep1->execute();
+    
     $queryPrep->setFetchMode(PDO::FETCH_ASSOC);
+    $queryPrep->setFetchMode(PDO::FETCH_ASSOC);
+    
+    while ($row = $queryPrep1->fetch())
+    {
+        echo $row['aantalvrouwen'];
+    }
     
     
     $verzendJSONData = '{ "persoonsgegevens" : [ ';
     foreach ($queryPrep->fetchAll() as $key => $value)
     {
-        $verzendJSONData .= '{ "id" : "'.$value["id"].'", "voornaam" : "'.$value["voornaam"].'", "tussenvoegsel" : "'.$value["tussenvoegsel"].'", "achternaam" : "'.$value["achternaam"].'"}, ';
-    }
-    $verzendJSONData .= ' {"id" : "", "voornaam" : "", "tussenvoegsel" : "", "achternaam" : ""}]}';
-    
-    
-    /*
-    $queryPrep->execute();
-    
-    while ($row = $queryPrep->fetch() )
-    {
-        echo $row["id"];
-    }
-    */  
+        
+            $verzendJSONData .= '{ "id" : "'.$value["id"].'", "voornaam" : "'.$value["voornaam"].'", "tussenvoegsel" : "'.$value["tussenvoegsel"].'", "achternaam" : "'.$value["achternaam"];
+            if ( $key == $queryPrep->rowCount() -1)
+            {
+                $verzendJSONData .= '"}]}';
+            }
+            else
+            {
+                $verzendJSONData .= '"}, ';
+            }
+    } 
 }
 catch(PDOException $e)
 {
